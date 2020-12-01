c454eb0ccd608753796f124d8e2673beee4525a3
J.Alexander <jasonalexx12@gmail.com>
2020-12-01 01:04:27 -0800

    Finished Game Of Life, retouch needed.

--- src/myPackage/GameOfLife.java
+++ src/myPackage/GameOfLife.java
@@ -1,8 +1,9 @@
 package myPackage;
 import java.util.Random;
+import java.lang.*;
 
 public class GameOfLife {
-    private static Integer[][] board;
+
 
     /*John Conway’s game of Life is a type of cellular automata that demonstrates
     how simple rules can lead to complex patterns or behaviors
@@ -17,6 +18,8 @@ public class GameOfLife {
     3. A live cell with > 3 neighbors dies (due to overpopulation).
     4. An empty cell with 3 neighbors becomes a live cell (from reproduction).*/
 
+
+    private static Integer[][] board;
 
     // creating an empty board for base of game with user defined parameters
     public static Integer[][] create_empty_world(int width, int length) {
@@ -45,47 +48,21 @@ public class GameOfLife {
 
     public static void print_world(Integer[][] filledBoard){
         //display output
-        for (int i=0;i<filledBoard.length; i++) {
-            for (int j=0; j < filledBoard[i].length; j++){
-                System.out.print(filledBoard[i][j] + " ");
-        }
+        for (Integer[] integers : filledBoard) {
+            for (Integer integer : integers) {
+                System.out.print(integer);
+            }
             System.out.println();
         }
     }
-
-    /*public int neighbourCount(int col, int row) {
-
-        int neighbourCounter = 0;
-
-        //find all serouding cell by adding +/- 1 to col and row
-        for (int colNum = col - 1 ; colNum <= (col + 1) ; colNum +=1  ) {
-
-            for (int rowNum = row - 1 ; rowNum <= (row + 1) ; rowNum +=1  ) {
-
-                //if not the center cell
-                if(! ((colNum == col) && (rowNum == row))) {
-
-                    //make sure it is within  grid
-                    if(withinBoard (colNum, rowNum)) {
-                        System.out.println("Neighbor of "+ col+ " "+ row + " - " + colNum +" " + rowNum );
-                        neighbourCounter++;
-                    }
-                }
-            }
-        }
-
-
-    }
-
-     */
 
     public static int neighborCount(int row, int col) {
 
         int count = 0;
         for(int i = row - 1; i <= row + 1; i++) {
-            if (i >= 0 && i < board.length) // fixed here
+            if (i >= 0 && i < board.length) // this code ensures the row does not exceed bounds
                 for(int j = col - 1; j <= col + 1; j++)
-                    if (j >= 0 && j < board[i].length) // fixed here
+                    if (j >= 0 && j < board[i].length) // collumn does not exceed bounds
                         if (i != row || j != col)
                             if (board[i][j] == 1)
                                 count++;
@@ -94,31 +71,76 @@ public class GameOfLife {
         return count;
     }
 
+    public static boolean isCellAlive(int x, int y){
+        return board[x][y] == 1;
+    }
+
+    public static void get_next_generation(){
+        int count;
+        for (int i = 0; i < board.length; i++){
+
+            for (int j = 0; j < board[i].length; j++){
+                count = neighborCount(i, j);
+                // System.out.print(isCellAlive(i, j));
+
+                // conditionals for determining cell future
+                // if more than 3 neighbours and is an alive cell
+                if (count > 3 && isCellAlive(i, j))
+                    board[i][j] = 0;
+
+                // if less than 2 neighbour and is an alive cell
+                else if (count < 2 && isCellAlive(i, j))
+                    board[i][j] = 0;
+
+                // if 3 neighbours are present and is a dead cell
+                else if (count == 3 && !isCellAlive(i, j))
+                    board[i][j] = 1;
+
+            }
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+
 
     // main function
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         // main driver code
-
+        int generation = 100;
+        int counting = 0;
         // creating empty world
-       board = create_empty_world(2, 2);
+       board = create_empty_world(10, 10);
 
         // fill board
         board = fill_world(board);
 
         // print out world
         print_world(board);
+        System.out.println("=================================");
+        Thread.sleep(500);
 
+        // loop world generation
+        while (counting <= generation){
+            get_next_generation();
+            print_world(board);
+            System.out.println("=================================");
+            counting++;
+            clearScreen();
+            Thread.sleep(500);
 
-        int count;
-        for (int i = 0; i < board.length; i++){
-
-            for (int j = 0; j < board[i].length; j++){
-                Integer[] row = board[i];
-
-                count = neighborCount(i, j);
-                System.out.print(count);
-            }
         }
+
+
+
+
+
+
+
+
 
 
 
